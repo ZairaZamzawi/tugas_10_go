2cc02fd729d05aebd7aec7e382b81639460b0ee1
Zaira Maulidin Zamzawi <zairazamzawi@gmail.com>
2020-07-04 23:12:14 +0700

    Tugas 10 Go
    Tugas 10 Golang untuk Niomic

---
+++ tugas10.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	time.Sleep(5 * time.Second)
+	fmt.Println("Nama Saya Zamzawi")
+	time.Sleep(5 * time.Second)
+	fmt.Println("Hobi Saya Ngoding")
+	time.Sleep(5 * time.Second)
+	fmt.Println("Tanggal Lahir Saya Rahasia")
+}
